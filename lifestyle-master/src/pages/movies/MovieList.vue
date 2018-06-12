6eea6736e1fca63c7534541eb4e7ef1efb7344b2
gloriousch <l>
2018-06-12 12:40:19 +0800

    影音页面

---
+++ lifestyle-master/src/pages/movies/MovieList.vue
@@ -0,0 +1,174 @@
+<template>
+<div>
+      <div class="header">
+      近期热映
+      <router-link to="/">
+        <div class="iconfont header-back">&#xe624;</div>
+      </router-link>
+  </div>
+    <div class="movieList">
+        <ul class="mv">
+            <li v-for="(obj,index) in movies" :key="index" @click="fn(obj.id)">
+                <div class="img-box">
+                    <img :src="obj.img" alt="">
+                </div>
+                <div class="info-box">
+                    <h2 class="title">{{obj.nm}}</h2>
+                    <p>{{obj.ver}}</p>
+                    <p>{{obj.star}}</p>
+                    <p>{{obj.showInfo}}</p>
+                </div>
+            </li>
+        </ul>
+        <div class="load" v-show="isLoad">
+            <img src="@/assets/img/loading.gif" alt="">
+        </div>
+        <div class="end" v-show="isEnd">
+            已经到底了
+        </div>
+    </div>
+     <bottom></bottom>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import Bottom from "../../common/Bottom";
+import MovieHeader from "./header";
+export default {
+  data() {
+    return {
+      movies: [],
+      isLoad: true,
+      isEnd: false,
+      flag: true
+    };
+  },
+  components: {
+    Bottom,
+    MovieHeader
+  },
+  methods: {
+    load() {
+      if (this.flag) {
+        this.flag = false;
+        axios
+          .get(
+            API_INTERFACE +
+              "http://m.maoyan.com/movie/list.json?type=hot&offset=" +
+              this.movies.length +
+              "&limit=10"
+          )
+          .then(res => {
+            if (res.data.data.movies < 10) {
+              this.isEnd = true;
+            }
+            console.log(res.data.data.movies);
+            this.movies = [...this.movies, ...res.data.data.movies];
+            this.isLoad = false;
+            this.flag = true;
+          })
+          .catch(() => {
+            console.log("请求失败");
+          });
+      }
+    },
+    fn(id) {
+      this.$router.push("/movieDetail/" + id);
+    }
+  },
+  mounted: function() {
+    //改变footer top 颜色 标题
+    this.$store.commit("change", { bgColor: "#f3f4f6", title: "Movie" });
+    //axios请求
+    this.load();
+    window.onscroll = () => {
+      var scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      var clientHeight = document.documentElement.clientHeight;
+      var scrollHeight = document.documentElement.scrollHeight;
+      var n = scrollTop + clientHeight;
+      console.log(Math.ceil(n), scrollHeight);
+      if (Math.ceil(n) == scrollHeight) {
+        if (!this.isEnd) {
+          this.isLoad = true;
+          this.load();
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.mv {
+    margin-top: 0.86rem;
+    margin-bottom: 0.96rem;
+}
+
+.header {
+    position: fixed;
+    overflow: hidden;
+    height: $headerHeight;
+    line-height: $headerHeight;
+    text-align: center;
+    color: #000000;
+    font-size: 0.32rem;
+    background: $bgColor;
+    top: 0;
+    left: 0;
+    width: 100%;
+    border-bottom: 0.02rem solid #d9d9d9;
+
+    .header-back {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 0.64rem;
+        text-align: center;
+        font-size: 0.4rem;
+        color: #000000;
+    }
+}
+
+.movieList li {
+    display: flex;
+    padding: 0.2rem;
+    border-bottom: 0.02rem solid #ccc;
+}
+
+.img-box {
+    flex-grow: 1;
+    width: 0;
+    margin-right: 0.2rem;
+}
+
+.img-box img {
+    width: 100%;
+}
+
+.info-box {
+    flex-grow: 2;
+    width: 0;
+}
+
+.title {
+    font-size: 0.33rem;
+    font-weight: bold;
+}
+
+.info-box p {
+    font-size: 0.27rem;
+    margin-top: 0.1rem;
+}
+
+.load {
+    text-align: center;
+}
+
+.end {
+    text-align: center;
+}
+</style>
